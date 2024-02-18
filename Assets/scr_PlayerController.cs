107b8328c2e5533699901e750a0cebd2022f314b
AshtonTankGallistel <Ashtontankgallistel@gmail.com>
2024-02-17 17:26:58 -0800

    Updated movement
    Player movement now matches camera direction

--- Assets/scr_PlayerController.cs
+++ Assets/scr_PlayerController.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class scr_PlayerController : MonoBehaviour
 {
     public Rigidbody rbSelf;
-    public Transform cameraTransform;
+    public float moveSpeed = 5f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,23 +18,34 @@ public class scr_PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //transform.localEulerAngles = new Vector3(0, cameraTransform.rotation.y, 0);
+
+        float forwardMovement = Input.GetAxis("Vertical");
+        float sideMovement = Input.GetAxis("Horizontal");
+
+        /***
         if (Input.GetKey(KeyCode.W) == true)
         {
-            rbSelf.AddForce(Vector3.forward);
+            //new Vector3(transform.rotation.y / 90,0,1 - transform.rotation.y / 90);
+            forwardMovement += ;
+            rbSelf.AddForce(-1 * transform.forward);
         }
         if (Input.GetKey(KeyCode.S) == true)
         {
-            rbSelf.AddForce(Vector3.back);
+            rbSelf.AddForce(transform.forward);
         }
         if (Input.GetKey(KeyCode.A) == true)
         {
-            rbSelf.AddForce(Vector3.left);
+            rbSelf.AddForce(-1*transform.right);
         }
         if (Input.GetKey(KeyCode.D) == true)
         {
-            rbSelf.AddForce(Vector3.right);
+            rbSelf.AddForce(transform.right);
         }
+        ***/
 
-        transform.localEulerAngles = new Vector3(0,cameraTransform.rotation.y,0);
+        Vector3 movedirection = forwardMovement * transform.forward + sideMovement * transform.right;
+        rbSelf.AddForce(movedirection * moveSpeed,ForceMode.Force);
+        //rbSelf.GetAccumulatedForce;
     }
 }
