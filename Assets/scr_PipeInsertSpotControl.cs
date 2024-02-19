e6f79fc35fd8ddfc77e7f7d68fea10ebe499e1fe
AshtonTankGallistel <Ashtontankgallistel@gmail.com>
2024-02-19 15:35:29 -0800

    Improved pipe stuff
    Changed gears to pipe stuff, should probably work now

---
+++ Assets/scr_PipeInsertSpotControl.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_PipeInsertSpotControl : MonoBehaviour
+{
+    public Quaternion pipeRotation;
+    public string requiredPipeType = "default";
+    public bool filled = false;
+    public bool working = false;
+
+    public void UpdateInsertion(string pipeType)
+    {
+        if (pipeType == requiredPipeType)
+        {
+            filled = true;
+            working = true;
+        }
+        else
+        {
+            filled = true;
+            working = false;
+        }
+
+    }
+
+    public void Removal() 
+    {
+        filled = false;
+        working = false;
+    }
+}
